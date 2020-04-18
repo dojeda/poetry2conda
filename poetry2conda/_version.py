6c38b82ce6986c8558c3240b7faa645e8a672fff
David Ojeda <david.ojeda@gmail.com>
2020-04-18 14:03:32 +0200

    Add package metadata, replace version with pkg_resources

--- poetry2conda/_version.py
+++ poetry2conda/_version.py
@@ -1,22 +1,13 @@
-import pathlib
 import warnings
 
-import toml
+import pkg_resources
 
 
 def get_version():
-    path = pathlib.Path(__file__).parents[1] / 'pyproject.toml'
     default = '0.0.0+unknown'
-    if not path.exists():
-        return default
-
     try:
-        with path.open('r') as f:
-            config = toml.load(f)
-
-        version = config['tool']['poetry']['version']
-        return version
-
+        distribution = pkg_resources.get_distribution('poetry2conda')
+        return distribution.version
     except Exception as ex:
         warnings.warn(f'Could not determine poetry2conda version: {ex}', UserWarning)
         return default
